1042ffb232ad0704fe864e639c2b9951b5bdefa8
Garrett Howard <hylander.garrett@gmail.com>
2020-03-27 14:07:22 -0600

    required HTML routes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 
-app.use(require("./routes/html-routes"));
+require("./routes/html-routes")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}! http://localhost:${PORT}`);
